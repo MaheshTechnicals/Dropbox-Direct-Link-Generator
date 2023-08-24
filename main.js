3323fbaf9fd2d62f1d89150a39cf136bde0e5fb4
msvarma10001@gmail.com <msvarma10001@gmail.com>
2023-08-24 13:01:38 +0530

    Some more element added

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('Hello World!');
+let input = document.querySelector("#input")
+let btn = document.querySelector("#btn")
+
+
+
+// Remeve placeholder on input click
+input.addEventListener("click", () => {
+  input.removeAttribute("placeholder")
+})
+
+
+
+// On click on button.
+btn.addEventListener("click", () => {
+
+  let val = input.value
+
+  if (val == "") {
+    alert("😔 Please Enter Link First.")
+  }
+
+  else {
+    Genrate()
+  }
+
+
+
+
+})
